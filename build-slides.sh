cea2e8c5365f6ac9427f66c87e0b2f95adde7c65
rmoff <robin@moffatt.me>
2025-10-21 15:46:27 +0100

    updated

--- build-slides.sh
+++ build-slides.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build reveal.js presentations
-find content/talks -name "slides.adoc" -type f | while read -r file; do
+find content/talk -name "slides.adoc" -type f | while read -r file; do
     dir=$(dirname "$file")
     echo "Building slides in $dir"
     asciidoctor-revealjs "$file" -D "$dir"
